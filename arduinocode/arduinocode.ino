1d41f61be502c41886c48a29d409904dd7440789
peytrick <46522201+peytrick@users.noreply.github.com>
2020-05-14 19:37:53 +0200

    Update arduinocode.ino
    Remove redundant text in the code.
     

--- arduinocode/arduinocode.ino
+++ arduinocode/arduinocode.ino
@@ -21,47 +21,15 @@
    Connect SDA to analog 4
    Connect VDD to 3.3-5V DC
    Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
 */
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
+// id, address
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-/*
-  void displaySensorDetails(void)
-  {
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-  }
-  /*
-  /**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
 void setup(void)
 {
   Serial.begin(115200);
@@ -70,34 +38,23 @@ void setup(void)
   /* Initialise the sensor */
   if (!bno.begin())
   {
-    /* There was a problem detecting the BNO055 ... check your connections */
+  /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while (1);
   }
 
   delay(1000);
-
   /* Use external crystal for better accuracy */
   bno.setExtCrystalUse(true);
-
-  /* Display some basic information on this sensor
-    displaySensorDetails();*/
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 void loop(void)
 {
   /* Get a new sensor event */
   sensors_event_t event;
   bno.getEvent(&event);
 
-
-    /* The processing sketch expects data as roll, pitch, yaw */
+  /* The processing sketch expects data as roll, pitch, yaw */
   Serial.print(event.orientation.y, 4);
   Serial.print(",");
   Serial.print(event.orientation.z, 4);
